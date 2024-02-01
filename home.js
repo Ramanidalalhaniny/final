9ece0cb20c143cfeb1caba8de1d54f935149cbcb
Ramanidalalhaniny <150138424+Ramanidalalhaniny@users.noreply.github.com>
2024-02-01 10:06:57 +0300

    Create home.js

---
+++ home.js
@@ -0,0 +1,18 @@
+<header class="header">
+  <nav class="header">
+    <ul>
+      <li>
+        <a href="/" class="isActive">Home</a>
+      </li>
+      <li>
+        <a href="/about" class="isActive">About</a>
+      </li>
+      <li>
+        <a href="/contact" class="isActive">Contact</a>
+      </li>
+    </ul>
+  </nav>
+  <div class="header_button">
+    <button class="searchBtn" aria-expanded="false">Search</button>
+  </div>
+</header>
